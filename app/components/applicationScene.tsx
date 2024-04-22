c5c199a52a73fd964f30fb26011f5aff0b9893af
Timelordregeneration10 <3497049745@qq.com>
2024-04-23 00:36:41 +0800

    RMT translate complete

---
+++ app/components/applicationScene.tsx
@@ -0,0 +1,9 @@
+
+
+export default function ApplicationScene() {
+    return (
+        <div className="h-[200vh] w-screen bg-application bg-cover bg-center lg:bg-[length:100vw_100vh] bg-fixed relative">
+            
+        </div>
+    )
+}
