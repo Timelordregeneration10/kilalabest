67bf41f3f189397ecfee6fb705ba6d1a550a50de
Timelordregeneration10 <3497049745@qq.com>
2024-10-10 16:25:18 +0800

    fix: camouflage title wrong

--- app/camouflage/layout.tsx
+++ app/camouflage/layout.tsx
@@ -1,7 +1,10 @@
-"use client";
 import Image from "next/image";
 import byuns_test from "./assets/byuns_test.jpg";
-import HeadProvider from "./provider/Head";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "BYUNSBoard",
+};
 
 export default function CamouflageLayout({
   children,
@@ -9,19 +12,17 @@ export default function CamouflageLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <HeadProvider>
-      <div className="w-full h-[2000vh] relative">
-        {/* bg */}
-        <Image
-          className="w-full object-cover absolute z-0 top-0 left-0"
-          width={2000}
-          height={8000}
-          alt="byuns_test"
-          src={byuns_test}
-        ></Image>
-        {/* content */}
-        <div className="w-full text-left relative z-[1]">{children}</div>
-      </div>
-    </HeadProvider>
+    <div className="w-full h-[2000vh] relative">
+      {/* bg */}
+      <Image
+        className="w-full object-cover absolute z-0 top-0 left-0"
+        width={2000}
+        height={8000}
+        alt="byuns_test"
+        src={byuns_test}
+      ></Image>
+      {/* content */}
+      <div className="w-full text-left relative z-[1]">{children}</div>
+    </div>
   );
 }
