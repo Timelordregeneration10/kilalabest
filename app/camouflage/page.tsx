7c898f3af77e5e12bca6faae03dacfeca6da2960
Timelordregeneration10 <3497049745@qq.com>
2024-10-11 10:11:56 +0800

    feat: swayleaf not overflow-hidden

--- app/camouflage/page.tsx
+++ app/camouflage/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import SwayLeaf from "../components/SwayLeaf";
+
 const CamouflagePage: React.FC = () => {
   return (
     <div className="w-full h-screen p-10 flex justify-around items-center">
-      camouflage
+      <SwayLeaf></SwayLeaf>
     </div>
   );
 };
